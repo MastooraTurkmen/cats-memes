d6b5912d5f4a83e821370ae997f779979bc02042
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-10-18 11:20:31 +0430

    Uptadate commit

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+// Here the images data, where i used the Postimg for images.
+
 export const catsData = [
   {
     emotionTags: ["moody"],
@@ -5,162 +7,189 @@ export const catsData = [
     image: "https://i.postimg.cc/KzhV2htG/angry.jpg",
     alt: "A cat looking moody",
   },
+
   {
     emotionTags: ["moody", "insomniac"],
     isGif: false,
     image: "https://i.postimg.cc/02XBNw7H/angry2.jpg",
     alt: "A cat looking moody",
   },
+
   {
     emotionTags: ["moody"],
     isGif: false,
     image: "https://i.postimg.cc/3NcF32zX/angry3.jpg",
     alt: "A cat looking moody",
   },
+
   {
     emotionTags: ["confused", "sad"],
     isGif: false,
     image: "https://i.postimg.cc/0jFDTPJZ/confused.jpg",
     alt: "A cat looking confused",
   },
+
   {
     emotionTags: ["dominant", "moody"],
     isGif: false,
     image: "https://i.postimg.cc/jjMwVtpN/dominant.jpg",
     alt: "A cat looking dominant",
   },
+
   {
     emotionTags: ["happy", "relaxed"],
     isGif: false,
     image: "https://i.postimg.cc/fWJqkvvn/happy.jpg",
     alt: "A cat looking happy",
   },
+
   {
     emotionTags: ["hungry"],
     isGif: false,
     image: "https://i.postimg.cc/XqCgLQ2r/hungry.jpg",
     alt: "A cat looking hungry",
   },
+
   {
     emotionTags: ["hungry"],
     isGif: false,
     image: "https://i.postimg.cc/k5YFdkzV/hungry1.jpg",
     alt: "A cat looking hungry",
   },
+
   {
     emotionTags: ["insomniac"],
     isGif: false,
     image: "https://i.postimg.cc/rmSm9418/insomnia.jpg",
     alt: "A cat looking insomniac",
   },
+
   {
     emotionTags: ["insomniac"],
     isGif: false,
     image: "https://i.postimg.cc/pdjX2j1D/insomnia1.jpg",
     alt: "A cat looking insomniac",
   },
+
   {
     emotionTags: ["relaxed"],
     isGif: false,
     image: "https://i.postimg.cc/j5KKtFfb/lazy.jpg",
     alt: "A cat looking lazy",
   },
+
   {
     emotionTags: ["scared"],
     isGif: false,
     image: "https://i.postimg.cc/L4xRFv6c/nervous.jpg",
     alt: "A cat looking nervous",
   },
+
   {
     emotionTags: ["sad"],
     isGif: false,
     image: "https://i.postimg.cc/nrP6jRXM/sad.jpg",
     alt: "A cat looking sad",
   },
+
   {
     emotionTags: ["sad", "moody"],
     isGif: false,
     image: "https://i.postimg.cc/28qpRDtW/sad1.jpg",
     alt: "A cat looking sad",
   },
+
   {
     emotionTags: ["moody"],
     isGif: true,
     image: "https://i.postimg.cc/HLvDDhBj/angry.gif",
     alt: "A cat looking moody",
   },
+
   {
     emotionTags: ["moody"],
     isGif: true,
     image: "https://i.postimg.cc/k5pZhtgb/angry2.gif",
     alt: "A cat looking angry",
   },
+
   {
     emotionTags: ["confused"],
     isGif: true,
     image: "https://i.postimg.cc/pL1WHCWG/confused2.gif",
     alt: "A cat looking confused",
   },
+
   {
     emotionTags: ["dominant"],
     isGif: true,
     image: "https://i.postimg.cc/8zkR3pcd/dominant.gif",
     alt: "A cat looking dominant",
   },
+
   {
     emotionTags: ["happy"],
     isGif: true,
     image: "https://i.postimg.cc/dVFH0c1D/happy.gif",
     alt: "A cat looking happy",
   },
+
   {
     emotionTags: ["hungry", "sad", "confused"],
     isGif: true,
     image: "https://i.postimg.cc/0NwdP6Cm/confused.gif",
     alt: "A cat looking hungry",
   },
+
   {
     emotionTags: ["hungry"],
     isGif: true,
     image: "https://i.postimg.cc/jSJxRNDR/hungry.gif",
     alt: "A cat looking hungry",
   },
+
   {
     emotionTags: ["hungry"],
     isGif: true,
     image: "https://i.postimg.cc/5y6cndvf/hungry2.gif",
     alt: "A cat looking hungry",
   },
+
   {
     emotionTags: ["insomniac", "scared"],
     isGif: true,
     image: "https://i.postimg.cc/MGDhwKmW/insomnia2.gif",
     alt: "A cat looking insomniac",
   },
+
   {
     emotionTags: ["relaxed"],
     isGif: true,
     image: "https://i.postimg.cc/LXPQX9zY/lazy.gif",
     alt: "A cat looking relaxed",
   },
+
   {
     emotionTags: ["relaxed"],
     isGif: true,
     image: "https://i.postimg.cc/B6JP8NFT/relaxed2.gif",
     alt: "A cat looking relaxed",
   },
+
   {
     emotionTags: ["scared", "sad"],
     isGif: true,
     image: "https://i.postimg.cc/q7wMFrtK/nervous.gif",
     alt: "A cat looking nervous",
   },
+
   {
     emotionTags: ["scared"],
     isGif: true,
     image: "https://i.postimg.cc/63Tw1vxs/nervous2.gif",
     alt: "A cat looking scared",
   },
+
   {
     emotionTags: ["sad"],
     isGif: true,
